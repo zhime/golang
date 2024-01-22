94b770bd7c350e2f604363f14c0ecd2ba142b872
dongchen <dongchen@netopstec.com>
2024-01-22 09:14:28 +0800

    1.注释和转义字符

--- base/1.注释和转义字符.go
+++ base/1.注释和转义字符.go
@@ -9,6 +9,13 @@ func Sum(num ...int) (sum int) {
 	}
 	return
 }
+
+type User struct {
+	Name    string
+	Age     int
+	Address string
+}
+
 func main() {
 	// 单行注释
 	fmt.Println(Sum(1, 2))
@@ -21,4 +28,26 @@ func main() {
 	// 转义字符
 	fmt.Println("\"hello world\"")
 	fmt.Println(`\"hello world\"`)
+
+	// 格式化字符串
+	/*
+		%v	按值的本来值输出
+		%+v	在 %v 基础上，对结构体字段名和值进行展开
+		%#v	输出 Go 语言语法格式的值
+		%T	输出 Go 语言语法格式的类型和值
+		%%	输出 % 本体
+		%b（oOdxX）	整型的不同进制方式显示
+		%U	Unicode 字符
+		%s	字符串
+		%d	整数
+		%f	浮点数
+		%p	指针，十六进制方式显示
+	*/
+	user := User{
+		Name:    "张三",
+		Age:     18,
+		Address: "hangzhou",
+	}
+	fmt.Println(user)
+	fmt.Printf("user 类型：%T\n", user)
 }
