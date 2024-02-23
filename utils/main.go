b9683b0c34511dff49ed082804d1ddf566ea605c
zhime <931013000@qq.com>
2024-02-23 16:28:51 +0800

    sysinfo

---
+++ utils/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"github.com/zhime/golang/utils/sys"
+)
+
+func main() {
+	ip := sys.GetHostIp()
+	fmt.Println(ip)
+
+	hostname := sys.GetHostName()
+	fmt.Println(hostname)
+
+	os := sys.GetOS()
+	fmt.Println(os)
+}
