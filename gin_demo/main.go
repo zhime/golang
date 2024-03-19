d6279ef3b3bbc348fe154622d3117a48f066862f
zhime <931013000@qq.com>
2024-03-19 10:16:42 +0800

    gin

--- gin_demo/main.go
+++ gin_demo/main.go
@@ -1,20 +1,28 @@
 package main
 
 import (
-	"fmt"
+	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/hello", func(writer http.ResponseWriter, request *http.Request) {
-		fmt.Println(request.Method)
-		fmt.Println(request.Host)
-		fmt.Println(request.Header)
-		_, _ = fmt.Fprintln(writer, "gin demo")
-	})
+	//http.HandleFunc("/hello", func(writer http.ResponseWriter, request *http.Request) {
+	//	fmt.Println(request.Method)
+	//	fmt.Println(request.Host)
+	//	fmt.Println(request.Header)
+	//	_, _ = fmt.Fprintln(writer, "gin demo")
+	//})
+	//
+	//err := http.ListenAndServe("0.0.0.0:9090", nil)
+	//if err != nil {
+	//	fmt.Println(err)
+	//}
 
-	err := http.ListenAndServe("0.0.0.0:9090", nil)
-	if err != nil {
-		fmt.Println(err)
-	}
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"msg": "hello",
+		})
+	})
+	_ = r.Run()
 }
